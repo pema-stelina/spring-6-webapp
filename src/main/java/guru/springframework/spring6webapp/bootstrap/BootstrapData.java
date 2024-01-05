cfe11ecabc6bf2b13f5d5026a81f9556583df59a
stelinapema <stelina@apptitude.nl>
2024-01-05 14:52:56 +0100

    save

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -51,6 +51,8 @@ public class BootstrapData implements CommandLineRunner {
 
         ericSaved.getBooks().add(dddSaved);
         rodSaved.getBooks().add(noEJBSaved);
+        dddSaved.getAuthors().add(ericSaved);
+        noEJBSaved.getAuthors().add(rodSaved);
 
 
         Publisher publisher = new Publisher();
@@ -69,6 +71,7 @@ public class BootstrapData implements CommandLineRunner {
         authorRepository.save(rodSaved);
         bookRepository.save(dddSaved);
         bookRepository.save(noEJBSaved);
+
 
 
         System.out.println("In Bootstrap");
